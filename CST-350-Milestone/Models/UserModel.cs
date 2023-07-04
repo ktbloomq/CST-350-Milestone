a3505db5032d5b52cfa4e5da29eb384bc80685ae
ktbloomq <ktb@bloomquist.com>
2023-07-04 10:42:37 -0700

    add user model

---
+++ CST-350-Milestone/Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CST_350_Milestone.Models
+{
+    public class UserModel
+    {
+        // Takes First Name, Last Name, Sex, Age, State, Email Address, Username, and Password as form fields.
+
+        public int Id { get; set; }
+
+        [Required]
+        public string firstName { get; set; }
+        
+        [Required]
+        public string lastName { get; set; }
+        
+        [Required]
+        public string sex { get; set; }
+        
+        [Required]
+        public int age { get; set; }
+        
+        [Required]
+        public string state { get; set; }
+        
+        [Required]
+        public string email { get; set; }
+        
+        [Required]
+        public string username { get; set; }
+        
+        [Required]
+        public string password { get; set; }
+    }
+}
