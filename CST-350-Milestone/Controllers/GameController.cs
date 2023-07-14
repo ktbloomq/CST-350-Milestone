9a833b9d126411e19729cfabd5610c763fe9a335
Chance Anderson <thejollycanadian@yahoo.com>
2023-07-13 18:07:58 -0600

    Added basic view for game

---
+++ CST-350-Milestone/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using CST_350_Milestone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CST_350_Milestone.Controllers
+{
+	public class GameController : Controller
+	{
+		public IActionResult Index(int difficulty, int size)
+		{
+			if (size == 0 && difficulty == 0)
+			{
+				size = 8;
+				difficulty = 1;
+			}
+			List<GameCellModel> cells = new List<GameCellModel>();
+
+			for (int i = 0; i < size * 10; i++)
+			{
+				cells.Add(new GameCellModel(i));
+			}
+			return View(cells);
+		}
+	}
+}
---
+++ CST-350-Milestone/Models/GameCellModel.cs
@@ -0,0 +1,26 @@
+﻿namespace CST_350_Milestone.Models
+{
+	public class GameCellModel
+	{
+		public int ID { get; set; }
+		public int Row { get; set; }
+		public int Column { get; set; }
+		public bool IsVisited { get; set; }
+		public bool Live { get; set; }
+		public int LiveNeighbors { get; set; }
+		public bool IsFlagged { get; set; }
+
+		public GameCellModel(int id) 
+		{ 
+			if (id < 10)
+			{
+				Row = 0; Column = id;
+			}
+			else
+			{
+				Row = id / 10; Column = id % 10;
+			}
+		}
+
+	}
+}
