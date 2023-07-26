0e54ea545a76b1198ea509cf58d7508e4654f46a
ktbloomq <ktb@bloomquist.com>
2023-07-26 12:59:19 -0700

    only run game js on game page

---
+++ CST-350-Milestone/wwwroot/js/minesweeper.js
@@ -0,0 +1,48 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    $(document).bind("contextmenu", function (e) {
+        e.preventDefault();
+    });
+    $(document).on("mousedown", ".game-button", function (event) {
+        event.preventDefault();
+        let buttonNumber = $(this).val();
+        switch (event.which) {
+            case 1:
+                // console.log("left");
+                doButtonUpdate(buttonNumber, "/Game/ShowOneButton");
+                break;
+            case 3:
+                // console.log("right");
+                doButtonUpdate(buttonNumber, "/Game/Flag");
+                break;
+            default:
+            // console.log("nothing");
+
+        }
+    });
+});
+let test;
+function doButtonUpdate(buttonNumber, urlString) {
+    $.ajax({
+        datatype: "json",
+        method: "POST",
+        url: urlString,
+        data: {
+            "buttonNumber": buttonNumber
+        },
+        success: function (data) {
+            //console.log(data);
+            test = data;
+            if (data.status === "lose") {
+                window.location.href = "/Game/Lose";
+            } else if (data.status === "win") {
+                window.location.href = "/Game/Win";
+            } else {
+                $("#" + buttonNumber).html(data);
+            }
+        }
+    });
+}
--- CST-350-Milestone/wwwroot/js/site.js
+++ CST-350-Milestone/wwwroot/js/site.js
@@ -2,47 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(function () {
-    $(document).bind("contextmenu", function (e) {
-        e.preventDefault();
-    });
-    $(document).on("mousedown", ".game-button", function (event) {
-        event.preventDefault();
-        let buttonNumber = $(this).val();
-        switch (event.which) {
-            case 1:
-                // console.log("left");
-                doButtonUpdate(buttonNumber, "/Game/ShowOneButton");
-                break;
-            case 3:
-                // console.log("right");
-                doButtonUpdate(buttonNumber, "/Game/Flag");
-                break;
-            default:
-                // console.log("nothing");
-
-        }
-    });
-});
-let test;
-function doButtonUpdate(buttonNumber, urlString) {
-    $.ajax({
-        datatype: "json",
-        method: "POST",
-        url: urlString,
-        data: {
-            "buttonNumber": buttonNumber
-        },
-        success: function (data) {
-            //console.log(data);
-            test = data;
-            if (data.status === "lose") {
-                window.location.href = "/Game/Lose";
-            } else if (data.status === "win") {
-                window.location.href = "/Game/Win";
-            } else {
-                $("#" + buttonNumber).html(data);
-            }
-        }
-    });
-}
+// minesweeper logic moved to minesweeper.js
