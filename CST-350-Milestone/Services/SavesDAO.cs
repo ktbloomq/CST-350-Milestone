4ccf9d37c5c1c0c67a876c80b79d6d42c844eeac
ktbloomq <ktb@bloomquist.com>
2023-08-03 12:02:55 -0700

    partial saves commit

---
+++ CST-350-Milestone/Services/SavesDAO.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.SqlClient;
+using CST_350_Milestone.Models;
+
+namespace CST_350_Milestone.Services
+{
+	public class SavesDAO
+	{
+		string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=milestone-cst-350;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+		// incomplete
+		public bool getOne(int id)
+		{
+			Console.WriteLine("DAO: " + id);
+			bool success = false;
+
+			string sqlStatement = "SELECT * FROM dbo.saves";
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+				//command.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+
+				try
+				{
+					connection.Open();
+					SqlDataReader reader = command.ExecuteReader();
+					if (reader.HasRows)
+					{
+						Console.WriteLine("has rows");
+						Console.WriteLine(reader.GetValue(0));
+						success = true;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+			return success;
+		}
+		public bool save(int userId, string gameState)
+		{
+			bool success = false;
+			string sqlStatement = "INSERT INTO dbo.saves (userId, game) Values(@0, @1)";
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					SqlCommand command = new SqlCommand(sqlStatement, connection);
+					command.Parameters.Add("@0", System.Data.SqlDbType.Int).Value = userId;
+					command.Parameters.Add("@1", System.Data.SqlDbType.NVarChar).Value = gameState;
+					connection.Open();
+					command.ExecuteNonQuery();
+					connection.Close();
+
+					success = true;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+			return success;
+		}
+	}
+}
